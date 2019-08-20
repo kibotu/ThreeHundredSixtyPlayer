be534eba78c45ab0618ef5f57a1a48543223cd28
Jan Rabe <jan.rabe@exozet.com>
2019-08-20 13:37:07 +0200

    fixed glide module

---
+++ ThreeHundertSixtyPlayer/src/main/java/com/exozet/threehundredsixtyplayer/ThreeSixtyAppGlideModule.kt
@@ -0,0 +1,6 @@
+package com.exozet.threehundredsixtyplayer
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+internal class ThreeSixtyAppGlideModule : AppGlideModule()
--- ThreeHundertSixtyPlayer/src/main/java/com/exozet/threehundredsixtyplayer/UriUtils.kt
+++ ThreeHundertSixtyPlayer/src/main/java/com/exozet/threehundredsixtyplayer/UriUtils.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Environment
-import com.bumptech.glide.Glide
 import com.bumptech.glide.Priority
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
@@ -15,26 +14,28 @@ import java.io.File
 
 fun String.parseAssetFile(): Uri = Uri.parse("file:///android_asset/$this")
 
-fun String.parseInternalStorageFile(context: Context): Uri = Uri.parse("${context.filesDir.absolutePath}/$this")
+fun String.parseInternalStorageFile(context: Context): Uri =
+    Uri.parse("${context.filesDir.absolutePath}/$this")
 
-fun String.parseExternalStorageFile(): Uri = Uri.parse("${Environment.getExternalStorageDirectory()}/$this")
+fun String.parseExternalStorageFile(): Uri =
+    Uri.parse("${Environment.getExternalStorageDirectory()}/$this")
 
 fun String.parseFile(): Uri = Uri.fromFile(File(this))
 
-fun Uri.loadImage(context: Context, block: (bitmap: Bitmap?) -> Unit) = Glide.with(context)
-        .asBitmap()
-        .load(this)
-        .apply(
-                RequestOptions
-                        .fitCenterTransform()
-                        .priority(Priority.HIGH)
-                        .dontAnimate()
-                        .skipMemoryCache(false)
-                        .override(2048, 2048)
-                        .diskCacheStrategy(DiskCacheStrategy.ALL)
-        )
-        .into(object : SimpleTarget<Bitmap>() {
-            override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
-                block(resource)
-            }
-        })
+fun Uri.loadImage(context: Context, block: (bitmap: Bitmap?) -> Unit) = GlideApp.with(context)
+    .asBitmap()
+    .load(this)
+    .apply(
+        RequestOptions
+            .fitCenterTransform()
+            .priority(Priority.HIGH)
+            .dontAnimate()
+            .skipMemoryCache(false)
+            .override(2048, 2048)
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
+    )
+    .into(object : SimpleTarget<Bitmap>() {
+        override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+            block(resource)
+        }
+    })
